90c1bc5970958186b117070d93b160951a889403
Skyler Mayfield <skyler544@gmail.com>
2024-11-02 19:53:52 +0100

    test quality function more thoroughly

--- tests/Products/AbstractProductTests.php
+++ tests/Products/AbstractProductTests.php
@@ -20,7 +20,18 @@ trait AbstractProductTests
         $this->assertEquals(9, $product->getSellIn());
     }
 
-    public function testQualityRange(): void
+    public function testModifyQualityNormally(): void
+    {
+        $product = $this->createProduct("Test Product", 10, 20);
+
+        $product->modifyQuality(2);
+        $this->assertEquals(22, $product->getQuality());
+
+        $product->modifyQuality(-4);
+        $this->assertEquals(18, $product->getQuality());
+    }
+
+    public function testQualityMaxAndMin(): void
     {
         $product = $this->createProduct("Test Product", 10, 20);
 
