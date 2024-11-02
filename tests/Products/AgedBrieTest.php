e5bae26079bd430c669d051abb316899b5a4ab7f
Skyler Mayfield <skyler544@gmail.com>
2024-11-02 20:13:21 +0100

    add tests for AgedBrie

--- tests/Products/AgedBrieTest.php
+++ tests/Products/AgedBrieTest.php
@@ -15,4 +15,24 @@ class AgedBrieTest extends TestCase
     ): AbstractProduct {
         return new AgedBrie($name, $sellIn, $quality);
     }
+
+    public function testQualityIncreasesNormallyWhenSellInGreaterZero(): void
+    {
+        $product = $this->createProduct("Aged Brie", 8, 5);
+
+        $product->updateQuality();
+        $this->assertEquals(6, $product->getQuality());
+        $product->updateQuality();
+        $this->assertEquals(7, $product->getQuality());
+    }
+
+    public function testQualityIncreasesTwiceAsFastWhenSellInLessThanZero(): void
+    {
+        $product = $this->createProduct("Aged Brie", 0, 5);
+
+        $product->updateQuality();
+        $this->assertEquals(7, $product->getQuality());
+        $product->updateQuality();
+        $this->assertEquals(9, $product->getQuality());
+    }
 }
